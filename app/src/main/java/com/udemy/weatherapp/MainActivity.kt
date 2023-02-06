0d936d56e89e2199d2c2245f19ffcf7819bfa5f9
DESKTOP-KGEJ707\mrhi03-13 <a43361904@gmai.com>
2023-02-06 13:41:22 +0900

    Retrofit 및 API 호출 위한 인터넷 연결 준비

--- app/src/main/java/com/udemy/weatherapp/MainActivity.kt
+++ app/src/main/java/com/udemy/weatherapp/MainActivity.kt
@@ -71,6 +71,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun getLocationWeatherDetails() {
+        if (Constants.isNetworkAvailable(this)) {
+            Toast.makeText(
+                this@MainActivity,
+                "You have connected to the internet. Now you can make an api call.",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                this@MainActivity, "No internet connection available.", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     private fun showRationalDialogForPermissions() {
         AlertDialog.Builder(this)
             .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
@@ -107,6 +121,8 @@ class MainActivity : AppCompatActivity() {
 
             val longitude = mLastLocation.longitude
             Log.i("Current Longitude", "$longitude")
+
+            getLocationWeatherDetails()
         }
     }
 
