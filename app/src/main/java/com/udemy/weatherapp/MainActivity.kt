627b209857651c1aed44c4eecc47461163b5f5c2
DESKTOP-KGEJ707\mrhi03-13 <a43361904@gmai.com>
2023-02-06 11:46:45 +0900

    Location Provider 실행 확인

--- app/src/main/java/com/udemy/weatherapp/MainActivity.kt
+++ app/src/main/java/com/udemy/weatherapp/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.udemy.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.udemy.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                this, "Your location provider is turned off. Please turn it on.", Toast.LENGTH_SHORT
+            ).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                this, "Your location provider is already turned on.", Toast.LENGTH_SHORT
+            ).show()
+        }
     }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
 }
